3c436e9e9e4ef0187c1edbcd12e5166d30531e94
muhammadnaeen262@gmail.com <muhammadnaeen262@gmail.com>
2025-07-21 09:33:52 -0400

    update the test

--- app/index.js
+++ app/index.js
@@ -6,8 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello from Jenkins!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// Only run server if script is executed directly (not during test)
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
 
-module.exports = app; // for testing
+module.exports = app;
